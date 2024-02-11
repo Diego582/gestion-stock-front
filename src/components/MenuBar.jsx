33b2470ec6c2720fd2f7c93306cf57bdc64e1b5d
Diego <penaomardiego@gmail.com>
2024-02-11 19:00:35 -0300

    Menu

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
 import React from "react";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import StoreIcon from "@mui/icons-material/Store";
 import GroupIcon from "@mui/icons-material/Group";
 import FeedIcon from "@mui/icons-material/Feed";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { Link } from "react-router-dom";
 
 export default function MenuBar() {
   const categories = ["Ventas", "Compras", "Clientes", "Reportes"];
@@ -16,9 +23,39 @@ export default function MenuBar() {
         height: "90vh",
         width: "5vw",
         color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
       }}
     >
-      a
+      <List>
+        {categories.map((text, index) => (
+          <ListItem key={index} disablePadding>
+            <Link to={text.toLowerCase()} style={{ textDecoration: "none" }}>
+              <ListItemButton>
+                <ListItemIcon>
+                  {text === "Ventas" ? (
+                    <ShoppingCartCheckoutIcon sx={{ color: "white" }} />
+                  ) : text === "Compras" ? (
+                    <StoreIcon sx={{ color: "white" }} />
+                  ) : text === "Clientes" ? (
+                    <GroupIcon sx={{ color: "white" }} />
+                  ) : (
+                    <FeedIcon sx={{ color: "white" }} />
+                  )}
+                </ListItemIcon>
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <ExitToAppIcon sx={{ color: "white" }} />
+          </ListItemIcon>
+        </ListItem>
+      </List>
     </Box>
   );
 }
---
+++ src/pages/Sales.jsx
@@ -0,0 +1,13 @@
+import Typography from "@mui/material/Typography";
+
+const Sales = () => {
+  return (
+    <>
+      <Typography variant="h1" color="initial">
+        ESTO ES Ventas
+      </Typography>
+    </>
+  );
+};
+
+export default Sales;
