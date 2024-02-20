183bb35b9fc51057fc1e2776cf63360ae3fa64c1
Diego <penaomardiego@gmail.com>
2024-02-20 09:48:25 -0300

    ventas1

---
+++ src/components/NavSales.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function NavSales() {
+  return (
+    <Box sx={{ width: "95vw", height: "10vh", backgroundColor: "red" }}>a</Box>
+  );
+}
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,12 +1,15 @@
 import Typography from "@mui/material/Typography";
+import NavSales from "../components/NavSales";
+import { Box } from "@mui/material";
 
 const Sales = () => {
   return (
-    <>
+    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+      <NavSales />
       <Typography variant="h1" color="initial">
         ESTO ES Ventas
       </Typography>
-    </>
+    </Box>
   );
 };
 
