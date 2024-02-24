7f5887648ea4b66785fbdc50a39338594d577a29
Diego <penaomardiego@gmail.com>
2024-02-24 20:57:18 -0300

    products2

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,17 +1,26 @@
 import { Box, Button, TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import TableProductos from "../components/TableProductos";
+import { useState } from "react";
 
 const Products = () => {
-  const handleOpenCloseCreate = () => {};
+  const [openCreate, setOpenCreate] = useState(false);
+
+  const handleOpenCloseCreate = () => {
+    setOpenCreate(!openCreate);
+  };
   const handleFilter = () => {};
 
   return (
     <Box
       sx={{
-        justifyContent: "space-between",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
         width: "100%",
         height: "90vh",
+        pl: 1,
+        pr: 1,
       }}
     >
       <Box
@@ -19,7 +28,8 @@ const Products = () => {
           display: "flex",
           justifyContent: "space-between",
           flexWrap: "wrap",
-         
+          mt: 1,
+          mb: 1,
         }}
       >
         <Typography variant="h4">Productos</Typography>
@@ -31,18 +41,18 @@ const Products = () => {
           Nuevo Producto
         </Button>
       </Box>
-      <TextField
-        id="filled-search"
-        label="Buscar Producto por Nombre"
-        type="search"
-        variant="filled"
-        name="lastName"
-        onKeyUp={handleFilter}
-        fullWidth
-        sx={{ mb: 2 }}
-      />
-
-      <TableProductos />
+      <Box sx={{ mt: 1, mb: 1 }}>
+        <TextField
+          id="filled-search"
+          label="Buscar Producto por Nombre"
+          type="search"
+          variant="filled"
+          name="lastName"
+          onKeyUp={handleFilter}
+          fullWidth
+        />
+      </Box>
+      <TableProductos openCreate={openCreate} setOpenCreate={setOpenCreate} />
     </Box>
   );
 };
