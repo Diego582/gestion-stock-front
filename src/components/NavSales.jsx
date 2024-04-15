57bb6b453c34b8d84e6c786a221b25398441915d
Diego582 <penaomardiego@gmail.com>
2024-04-15 00:25:53 -0300

    Sales

--- src/components/NavSales.jsx
+++ src/components/NavSales.jsx
@@ -1,24 +1,11 @@
-import {
-  Box,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import { Box, IconButton, Tooltip } from "@mui/material";
 import React from "react";
-import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
-import StoreIcon from "@mui/icons-material/Store";
-import GroupIcon from "@mui/icons-material/Group";
-import FeedIcon from "@mui/icons-material/Feed";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import CastConnectedIcon from "@mui/icons-material/CastConnected";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
 import { Link } from "react-router-dom";
 
 export default function NavSales() {
-  const categories = ["Ventas", "Compras", "Clientes", "Reportes"];
+  const categories = ["Remitos", "Facturaciones"];
 
   return (
     <Box
@@ -32,19 +19,18 @@ export default function NavSales() {
       }}
     >
       {categories.map((text, index) => (
-        <Box key={index} >
-          <Link to={text.toLowerCase()} style={{ textDecoration: "none" }}>
+        <Box key={index}>
+          <Link
+            to={"/" + text.toLowerCase()}
+            style={{ textDecoration: "none" }}
+          >
             <Box>
               <Tooltip title={text}>
                 <IconButton sx={{}}>
-                  {text === "Ventas" ? (
-                    <ShoppingCartCheckoutIcon sx={{ color: "white" }} />
-                  ) : text === "Compras" ? (
-                    <StoreIcon sx={{ color: "white" }} />
-                  ) : text === "Clientes" ? (
-                    <GroupIcon sx={{ color: "white" }} />
+                  {text === "Remitos" ? (
+                    <FactCheckIcon sx={{ color: "white" }} />
                   ) : (
-                    <FeedIcon sx={{ color: "white" }} />
+                    <CastConnectedIcon sx={{ color: "white" }} />
                   )}
                 </IconButton>
               </Tooltip>
---
+++ src/components/TableSales.jsx
@@ -0,0 +1,156 @@
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  MenuItem,
+  Modal,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import CreateProduct from "./CreateProduct";
+import product_actions from "../store/actions/products";
+import DeleteProduct from "./DeleteProduct";
+import EditProduct from "./EditProduct";
+
+const { read_products } = product_actions;
+
+export default function TableSales({ openCreate, setOpenCreate }) {
+  /*   const data = useSelector((store) => store[dataStore][dataStore]); */
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store.products.products);
+  const [product, setProduct] = useState("");
+  const [codigoBarras, setCodigoBarras] = useState({});
+  const [openDelete, setOpenDelete] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+
+  const columns = [
+    {
+      field: "codigoBarras",
+      headerName: "Código deBarras",
+    },
+    {
+      field: "descripcion",
+      headerName: "Descripcion",
+    },
+    {
+      field: "prices",
+      headerName: "Precio",
+    },
+    { field: "actions", headerName: "Acciones" },
+  ];
+
+  const [producto, setProducto] = useState({});
+  const handleSelected = (product, option) => {
+    setProduct(product);
+    option === "Edit" ? handleOpenCloseEdit() : handleOpenCloseDelete();
+  };
+
+  const handleOpenCloseEdit = () => {
+    setOpenEdit(!openEdit);
+  };
+
+  const handleOpenCloseDelete = () => {
+    setOpenDelete(!openDelete);
+  };
+
+  useEffect(() => {
+    /* dispatch(read_products()); */
+  }, []);
+
+  return (
+    <>
+      {data && data.length > 0 ? (
+        <TableContainer sx={{ maxHeight: "70vh" }}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {columns.map((column, index) => {
+                  return (
+                    <TableCell key={index} sx={{ p: 2 }}>
+                      {column.headerName}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((item, index) => {
+                return (
+                  <TableRow
+                    sx={{
+                      borderRadius: "10px",
+                      backgroundColor:
+                        index % 2 === 0 ? "rgba(157, 85, 82, 0.3)" : "white",
+                    }}
+                    key={index}
+                  >
+                    {columns.map((column, colIndex) => {
+                      if (column.field == "prices") {
+                        return (
+                          <TableCell key={colIndex} sx={{ p: 0, pl: 2 }}>
+                            {"$ "}
+                            {item.prices[0].value &&
+                              item.prices[0].value.toFixed(2)}
+                          </TableCell>
+                        );
+                      }
+                      if (column.field != "actions") {
+                        return (
+                          <TableCell key={colIndex} sx={{ p: 0, pl: 2 }}>
+                            {item[column.field]}
+                          </TableCell>
+                        );
+                      }
+                    })}
+                    <TableCell>
+                      <Box>
+                        <IconButton
+                          onClick={() => handleSelected(item, "Edit")}
+                        >
+                          <EditIcon color="info" />
+                        </IconButton>
+                        <IconButton
+                          onClick={() => handleSelected(item, "Delete")}
+                        >
+                          <DeleteIcon color="error" />
+                        </IconButton>
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Typography>No hay datos que mostrar</Typography>
+      )}
+
+      <CreateProduct openCreate={openCreate} setOpenCreate={setOpenCreate} />
+      <DeleteProduct
+        openDelete={openDelete}
+        setOpenDelete={setOpenDelete}
+        product={product}
+        setProduct={setProduct}
+      />
+
+      <EditProduct
+        openEdit={openEdit}
+        setOpenEdit={setOpenEdit}
+        product={product}
+        setProduct={setProduct}
+      />
+    </>
+  );
+}
