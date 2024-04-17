5b0cb1f2420cfc4c0436ebbe096e1a359eb1e337
Diego582 <penaomardiego@gmail.com>
2024-04-16 21:55:10 -0300

    Remitos

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -1,17 +1,36 @@
-import { Box, Button, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
 import NavSales from "../components/NavSales";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CrudSales from "../components/CrudSales";
 import TableSales from "../components/TableSales";
+import LocalPrintshopIcon from "@mui/icons-material/LocalPrintshop";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import SaveIcon from "@mui/icons-material/Save";
+import check_actions from "../store/actions/check";
+import { useDispatch, useSelector } from "react-redux";
+
+const { read_checks } = check_actions;
+
 
 const Check = () => {
+  const dispatch = useDispatch();
+
   const handleOpenCloseCreate = () => {};
 
   const [currentDate] = useState(new Date());
-  const [total] = useState(0);
+  const [total, setTotal] = useState(0);
   const [comprobante] = useState(0);
 
+  const checks = useSelector((store) => store.checks);
+
   console.log(currentDate, "currentdate");
+  console.log(checks, "storeCheck");
+
+  useEffect(() => {
+    dispatch(read_checks());
+  }, []);
+
+
   return (
     <Box
       sx={{
@@ -27,22 +46,31 @@ const Check = () => {
         sx={{
           display: "flex",
           justifyContent: "space-around",
+          alignItems: "center",
           flexWrap: "wrap",
           mt: 1,
           mb: 1,
         }}
       >
-        <Typography variant="h5">Comprobante N°: {comprobante}</Typography>
+        <Typography variant="h5">Ticket N°: 1-{comprobante}</Typography>
         <Typography variant="h5">
           Fecha: {currentDate.getDate()}/{currentDate.getMonth()}/
           {currentDate.getFullYear()}
         </Typography>
-        <Typography variant="h5">Monto: {total}</Typography>
+        <Typography variant="h5">Monto: $ {total.toFixed(2)}</Typography>
+        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+          <IconButton>
+            <AddBoxIcon fontSize="large" />
+          </IconButton>
+          <IconButton>
+            <LocalPrintshopIcon fontSize="large" />
+          </IconButton>
+        </Box>
       </Box>
       <Divider />
       <CrudSales />
       <Divider />
-      <TableSales />
+      <TableSales total={total} setTotal={setTotal} />
     </Box>
   );
 };
---
+++ src/store/actions/check.js
@@ -0,0 +1,109 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import apiUrl from "../../utils/apiUrl";
+
+const read_checks = createAsyncThunk("read_checks", async (obj) => {
+  //callback que realiza la petición
+
+  try {
+
+    let data = await axios(apiUrl + "checks");
+    console.log(data, 'data en actions')
+    return {
+      checks: data.data.response,
+    };
+  } catch (error) {
+
+    return {
+      checks: [],
+    };
+  }
+});
+
+const read_check = createAsyncThunk("read_check", async (obj) => {
+  //callback que realiza la petición
+  try {
+
+    let data = await axios(
+      apiUrl + "checks"
+    );
+
+    return {
+      check: data.data.response,
+    };
+  } catch (error) {
+    return {
+      check: {},
+    };
+  }
+});
+
+const create_check = createAsyncThunk(
+  "create_check", //nombre de la accion
+  async (obj) => {
+    //callback que realiza la petición
+    try {
+
+      let data = await axios.post(apiUrl + "checks", obj);
+      return {
+        check: data.data.response,
+        messages: [],
+      };
+    } catch (error) {
+      return {
+        check: false,
+        messages: error.response.data.messages || [error.response.data.message],
+      };
+    }
+  }
+);
+
+const update_check = createAsyncThunk(
+  "update_check", //nombre de la accion
+  async (obj) => {
+    //callback que realiza la petición
+    try {
+
+      let data = await axios.put(apiUrl + "checks/" + obj._id, obj.data);
+      return {
+        check: data.data.response,
+        messages: [],
+      };
+    } catch (error) {
+      return {
+        check: false,
+        messages: error.response.data.messages || [error.response.data.message],
+      };
+    }
+  }
+);
+
+
+const destroy_check = createAsyncThunk(
+  "destroy_check", //nombre de la accion
+  async (obj) => {
+    //callback que realiza la petición
+    try {
+      let data = await axios.delete(apiUrl + "checks/" + obj._id);
+
+      return {
+        check: data.data.response,
+        messages: [],
+      };
+    } catch (error) {
+      return {
+        customer: {},
+        messages: error.response.data.messages || [error.response.data.message],
+      };
+    }
+  }
+);
+
+const check_actions = {
+  read_checks,
+  read_check,
+  create_check,
+  destroy_check,
+  update_check
+};
+export default check_actions;
